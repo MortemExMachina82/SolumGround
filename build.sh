9f9bf27a6415436146b122a1ddbef713e311f8b0
MortemExMachina <me@ziasi.info>
2022-02-14 19:29:28 -0700

    fixed classpath to work with dependencies in lib

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 set -e
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd src
-javac --release 8 org/solumground/Main.java -d ../classes
-cd ../classes
-jar cfm ../SolumGround.jar ../assets/MANIFEST.MF ./*
-cd ../
-chmod +x SolumGround.jar
-java -jar SolumGround.jar
+javac -cp ".:../lib/json-simple/*:../lib/lwjgl/*:../lib/lwjgl-glfw/*:../lib/lwjgl-opengl/*" --release 8 org/solumground/Main.java -d out/production/SolumGround
+#cd ../
+#jar cfm SolumGround.jar ./out/production/SolumGround/META-INF/MANIFEST.MF ./out/production/SolumGround/*
+#cd ../
+#chmod +x SolumGround.jar
+#java -jar SolumGround.jar
+
